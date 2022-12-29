6f3d713fee084e69b767ceba7401bac6dda9c308
Alejandro Coria <alejandrocoria2009@gmail.com>
2022-12-29 19:45:00 -0300

    added the options texturePositionX, texturePositionY, textureScaleX and textureScaleY

--- leaflet-polygon.fillPattern-canvas.js
+++ leaflet-polygon.fillPattern-canvas.js
@@ -11,7 +11,40 @@
 
                 var options = layer.options
 
-                if (options.fill) {
+                if (options.imgId && options.textureScaleX !== 0 && options.textureScaleY !== 0) {
+                    const img = new Image();
+                    img.src = options.imgId
+
+                    const offscreen = new OffscreenCanvas(img.width, img.height);
+                    const ctxOffscreen = offscreen.getContext("2d")
+                    const patternOffscreen = ctxOffscreen.createPattern(img, 'repeat')
+                    ctxOffscreen.globalCompositeOperation = "copy"
+                    ctxOffscreen.globalAlpha = 1
+                    ctxOffscreen.fillStyle = patternOffscreen
+                    ctxOffscreen.fillRect(0, 0, img.width, img.height)
+                    ctxOffscreen.globalCompositeOperation = "multiply"
+                    ctxOffscreen.fillStyle = options.fillColor || options.color
+                    ctxOffscreen.fillRect(0, 0, img.width, img.height)
+
+                    if (offscreen.width != 0 && offscreen.height != 0) {
+                        ctx.save() // so we can remove the clipping
+                        ctx.clip()
+                        ctx.globalAlpha = options.fillOpacity
+                        ctx.imageSmoothingEnabled = false
+                        const bounds = layer._rawPxBounds
+                        const size = bounds.getSize()
+                        const pattern = ctx.createPattern(offscreen, 'repeat')
+                        const zoomScale = Math.pow(2, this._map.getZoom())
+                        const pixelOrigin = this._map.getPixelOrigin()
+                        const matrix = new DOMMatrix()
+                        matrix.translateSelf(options.texturePositionX * zoomScale - pixelOrigin.x, options.texturePositionY * zoomScale - pixelOrigin.y)
+                        matrix.scaleSelf(options.textureScaleX * zoomScale, options.textureScaleY * zoomScale, 1)
+                        pattern.setTransform(matrix)
+                        ctx.fillStyle = pattern
+                        ctx.fillRect(bounds.min.x, bounds.min.y, size.x, size.y)
+                        ctx.restore()
+                    }
+                } else {
                     ctx.globalAlpha = options.fillOpacity
                     ctx.fillStyle = options.fillColor || options.color
 
@@ -29,21 +62,8 @@
                     ctx.lineCap = options.lineCap
                     ctx.lineJoin = options.lineJoin
                     ctx.stroke()
-                    if (options.imgId) {
-
-                        var img = document.getElementById(options.imgId)
-                        ctx.save() // so we can remove the clipping
-                        ctx.clip()
-                        var bounds = layer._rawPxBounds
-                        var size = bounds.getSize()
-                        var pattern = ctx.createPattern(img, 'repeat')
-                        ctx.fillStyle = pattern
-                        ctx.fillRect(bounds.min.x, bounds.min.y, size.x, size.y)
-                        ctx.restore()
-                    }
                 }
             }
         })
     }
 }(this, document))
-
